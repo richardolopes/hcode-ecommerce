af04948b6dd0c97e1ece8758b6aa9a923cad9d46
richardolopes <richardxlopes@gmail.com>
2018-07-20 17:19:07 -0300

    Aula 105 - recuperar senha

---
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Mailer {
+
+	const USERNAME = "richlopes714@gmail.com";
+	const PASSWORD = "<?senha?>";
+	const NAME_FROM = "Hcode Store";
+
+	private $mail;
+
+	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
+	{
+		$config = array(
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+		);
+
+		Tpl::configure( $config );
+
+		$tpl = new Tpl;
+
+		foreach ($data as $key => $value) {
+			$tpl->assign($key, $value);
+		}
+
+		$html = $tpl->draw($tplName, true);
+
+		$this->mail = new \PHPMailer;
+
+		$this->mail->isSMTP();
+
+		$this->mail->SMTPDebug = 0;
+
+		$this->mail->Host = 'smtp.gmail.com';
+
+		$this->mail->Port = 587;
+
+		$this->mail->SMTPSecure = 'tls';
+
+		$this->mail->SMTPAuth = true;
+
+		$this->mail->Username = Mailer::USERNAME; // RICO L 714
+
+		$this->mail->Password = Mailer::PASSWORD;
+
+		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
+
+		$this->mail->addAddress($toAddress, $toName);
+
+		$this->mail->Subject = $subject;
+
+		$this->mail->msgHTML($html);
+
+		$this->mail->AltBody = 'Teste.';
+
+		function save_mail($this->mail)
+		{
+		    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+
+		    $imapStream = imap_open($path, $this->mail->Username, $this->mail->Password);
+
+		    $result = imap_append($imapStream, $path, $this->mail->getSentMIMEMessage());
+		    imap_close($imapStream);
+
+		    return $result;
+		}
+	}
+
+	public function send()
+	{
+		return $this->mail->send();
+	}
+}
+
+
+?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,7 @@ use \Hcode\Model;
 class User extends Model
 {
 	const SESSION = "User";
+	const SECRET = "HcodePHP7_Secret";
 
 	public static function login($login, $password)
 	{
@@ -33,7 +34,9 @@ class User extends Model
 
 			return $user;
 
-		} else {
+		}
+		else
+		{
 			throw new \Exception("Usuário inexistente ou senha inválida.");	
 		}
 	}
@@ -77,7 +80,7 @@ class User extends Model
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
 		));
-
+		
 		$this->setData($results[0]);
 	}
 
@@ -112,6 +115,97 @@ class User extends Model
 		$sql = new Sql();
 		$sql->query("CALL sp_users_delete(:iduser)", array(
 			":iduser"=>$this->getiduser()
+		));
+	}
+
+	public static function getForgot($email)
+	{
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT * FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :email;
+		", array(
+			":email"=>$email
+		));
+
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			$data = $results[0];
+			$resultsRecovery = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser"=>$data["iduser"],
+				"desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if (count($resultsRecovery) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha.");
+			}
+			else
+			{
+				$dataRecovery = $resultsRecovery[0];
+				// Função mcrypt_encrypt() depreciada no PHP 7.1 -> openssl_encrypt()
+				$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+
+				$link = "http://www.ecommerce.com.br/admin/forgot/reset?code=$code";
+
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha", "forgot", array(
+					"name"=>$data["desperson"],
+					"link"=>$link
+				));
+
+				$mailer->send();
+
+				return $data;
+			}
+		}
+	}
+
+	public static function validForgotDecrypt($code)
+	{
+		$idrecovery = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, User::SECRET, base64_decode($code), MCRYPT_MODE_ECB);
+
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT * FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b using(iduser)
+			INNER JOIN tb_persons c using(idperson)
+			WHERE a.idrecovery = :idrecovery
+			AND
+			a.dtrecovery IS NULL
+			AND
+			DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+			", array(
+				":idrecovery"=>$idrecovery
+			));
+
+		if (count($results) === 0) {
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+			return $results[0];
+		}
+	}
+
+	public static function setForgotUsed($idrecovery)
+	{
+		$sql = new Sql();
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+			"idrecovery"=>$idrecovery
+		));
+	}
+
+	public function setPassword($password)
+	{
+		$sql = new Sql();
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+			":password"=>$password,
+			"iduser"=>$this->getiduser()
 		));
 	}
 }
