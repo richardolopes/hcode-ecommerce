371c1bef60c4accfbd332da4aa60803624dcb100
richardolopes <richardxlopes@gmail.com>
2018-07-20 18:07:02 -0300

    Aula 105 - mailer 5.2

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -18,7 +18,7 @@ class Mailer {
 			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/email/",
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false
-		);
+	    );
 
 		Tpl::configure( $config );
 
@@ -31,50 +31,27 @@ class Mailer {
 		$html = $tpl->draw($tplName, true);
 
 		$this->mail = new \PHPMailer;
-
 		$this->mail->isSMTP();
-
 		$this->mail->SMTPDebug = 0;
-
+		$this->mail->Debugoutput = 'html';
 		$this->mail->Host = 'smtp.gmail.com';
-
 		$this->mail->Port = 587;
-
 		$this->mail->SMTPSecure = 'tls';
-
 		$this->mail->SMTPAuth = true;
-
-		$this->mail->Username = Mailer::USERNAME; // RICO L 714
-
+		$this->mail->Username = Mailer::USERNAME;
 		$this->mail->Password = Mailer::PASSWORD;
-
 		$this->mail->setFrom(Mailer::USERNAME, Mailer::NAME_FROM);
-
 		$this->mail->addAddress($toAddress, $toName);
-
 		$this->mail->Subject = $subject;
-
 		$this->mail->msgHTML($html);
-
-		$this->mail->AltBody = 'Teste.';
-
-		function save_mail($mail)
-		{
-		    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-
-		    $imapStream = imap_open($path, $this->mail->Username, $this->mail->Password);
-
-		    $result = imap_append($imapStream, $path, $this->mail->getSentMIMEMessage());
-		    imap_close($imapStream);
-
-		    return $result;
-		}
+		$this->mail->AltBody = 'This is a plain-text message body';
 	}
 
 	public function send()
 	{
 		return $this->mail->send();
 	}
+
 }
 
 
