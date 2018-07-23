ad1bd6f39c013883851fdb83600544251d9af41b
richardolopes <richardxlopes@gmail.com>
2018-07-23 05:17:40 -0300

    Aula 111 - paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -92,6 +92,31 @@ class Category extends Model
 		}
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 8)
+	{
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS * FROM
+			tb_products a 
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct 
+			INNER JOIN tb_categories c on c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+		", array(
+			":idcategory"=>$this->getidcategory()
+		));
+
+		$resultsItems = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return array(
+			"data"=>Product::checkList($results),
+			"total"=>(int)$resultsItems[0]["nrtotal"],
+			"pages"=>ceil($resultsItems[0]["nrtotal"] / $itemsPerPage)
+		);
+	}
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
