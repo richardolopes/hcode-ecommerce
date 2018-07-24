f575352df50afb9bd16c2bb72dff94773c4a368f
richardolopes <richardxlopes@gmail.com>
2018-07-24 00:33:16 -0300

    Aula 114 - carrinho produtos

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -90,6 +90,54 @@ class Cart extends Model
 		$this->setData($results[0]);
 	}
 
+	public function addProduct(Product $product)
+	{
+		$sql = new Sql();
+
+		$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES (:idcart, :idproduct)", array(
+			":idcart"=>$this->getidcart(),
+			"idproduct"=>$product->getidproduct()
+		));
+	}
+
+	public function removeProduct(Product $product, $all = false)
+	{
+		$sql = new Sql();
+
+		if ($all)
+		{
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL", array(
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			));
+		}
+		else
+		{
+			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1", array(
+				":idcart"=>$this->getidcart(),
+				":idproduct"=>$product->getidproduct()
+			));
+		}
+	}
+
+	public function getProducts()
+	{
+		$sql = new Sql();
+
+		$rows = $sql->select("SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl, COUNT(*) AS nrqtd, SUM(b.vlprice) AS vltotal
+			FROM tb_cartsproducts a 
+			INNER JOIN tb_products b 
+			ON a.idproduct = b.idproduct 
+			WHERE a.idcart = :idcart 
+			AND dtremoved IS NULL 
+			GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheight, b.vllength, b.vlweight, b.desurl
+			ORDER BY b.desproduct", array(
+				":idcart"=>$this->getidcart()
+		));
+
+		return Product::checkList($rows);
+	}
+
 }
 
 ?>
